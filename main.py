4a251d02983b64125f7aeb75dbc0bc8ca8138c81
SNESEAR <aer2201@bk.ru>
2022-03-23 00:32:37 +0500

    Modify main.py: add parse_command functionality

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #!./venv/bin/python
+from utils import parse_command, ArgError
+
+
+# TODO: add crptsploit command execution
+# TODO: add bash command parsing and execution
+
 def get_command():
     command = input("crptsploit> ")
-    if command == "exit":
-        return 0
     return command
 
 
@@ -10,11 +14,14 @@ def main():
     while True:
         try:
             command = get_command()
-            if command == 0:
+            if command == "exit":
                 return
-            # parse_command(command)
+            parse_command(command)
+            print(command)
         except KeyboardInterrupt:
             print("\nType 'exit' to quit")
+        except ArgError as err:
+            print(str(err))
 
 
 main()

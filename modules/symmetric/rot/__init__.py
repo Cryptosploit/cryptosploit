4c86a8b021047ecc547b48b727efbd7bf86cc9fb
SNESEAR <aer2201@bk.ru>
2022-04-06 22:23:57 +0500

    Add rot module

--- modules/symmetric/rot/__init__.py
+++ modules/symmetric/rot/__init__.py
@@ -1,8 +1,38 @@
+import string
+
 from modules import BaseModule
 
 
 class Rot(BaseModule):
-    ...
+    def encrypt(self):
+        plaintext = ""
+        key = int(self.env.get_var("key"))
+        alphabet = self.env.get_var("alphabet")
+        for letter in self.env.get_var("input"):
+            if letter in alphabet:
+                plaintext += alphabet[(alphabet.find(letter) + key) % len(alphabet)]
+            else:
+                plaintext += letter
+        print("[RESULT]", plaintext)
+
+    def decrypt(self):
+        ...
+
+    def attack(self):
+        ...
+
+    def run(self):
+        if not self.env.get_var("key").isdigit():
+            print("Key must be a natural number!")
+        match self.env.get_var("mode"):
+            case "attack":
+                self.attack()
+            case "decrypt":
+                self.decrypt()
+            case "encrypt":
+                self.encrypt()
+            case _:
+                print("No such mode!")
 
 
 module = Rot()

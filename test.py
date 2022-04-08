576bc2419f091df056e2c32ebb76dcf36bdae07b
y73n0k <konal2004@gmail.com>
2022-04-08 21:50:18 +0500

    Create setup for submodule

---
+++ test.py
@@ -0,0 +1,9 @@
+#!./venv/bin/python
+
+# from importlib import import_module
+# import sys
+#
+# sys.path.append("/home/y73n0k/Desktop/smth/projects/cryptosploit/cryptosploit-modules/src")
+#
+# module = import_module("cryptosploit_modules.symmetric.rot").module
+# print(module.env)
